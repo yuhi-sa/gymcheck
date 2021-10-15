84fe91eb4f9ff3891bfde06c3e8156d0b000995e
yuhi-sa <62089243+yuhi-sa@users.noreply.github.com>
2021-10-15 14:27:25 +0900

    Update test.py

--- test.py
+++ test.py
@@ -2,15 +2,10 @@ from src.check_env import check_env
 
 MAX_TIME = 2000
 ENVIRONMENTS = [
-    {"env_name":"CartPole","env_id": "CartPoleContinuousBulletEnv-v0", "video_length": 500,"IS_PLOT":False, "IS_LEARN":False, "learn_episode":0},
-    {"env_name":"InvertedPendulum", "env_id": "InvertedPendulumBulletEnv-v0", "video_length": 500, "IS_PLOT":False, "IS_LEARN":False, "learn_episode":0},
-    {"env_name":"Swingup", "env_id": "InvertedPendulumSwingupBulletEnv-v0", "video_length": 500, "IS_PLOT":False, "IS_LEARN":False, "learn_episode":0},
-    {"env_name":"DoublePendulum","env_id": "InvertedDoublePendulumBulletEnv-v0", "video_length": 500, "IS_PLOT":False, "IS_LEARN":False, "learn_episode":0},
-
-    # {"env_name":"CartPole","env_id": "CartPoleContinuousBulletEnv-v0", "video_length": 500,"IS_PLOT":True, "IS_LEARN":True, "learn_episode":1000000},
-    # {"env_name":"InvertedPendulum", "env_id": "InvertedPendulumBulletEnv-v0", "video_length": 500, "IS_PLOT":True, "IS_LEARN":True, "learn_episode":1000000},
-    # {"env_name":"Swingup", "env_id": "InvertedPendulumSwingupBulletEnv-v0", "video_length": 500, "IS_PLOT":True, "IS_LEARN":True, "learn_episode":1000000},
-    # # {"env_name":"DoublePendulum","env_id": "InvertedDoublePendulumBulletEnv-v0", "video_length": 500, "IS_PLOT":True, "IS_LEARN":True, "learn_episode":100000},
+    {"env_name":"CartPole","env_id": "CartPoleContinuousBulletEnv-v0"},
+    {"env_name":"InvertedPendulum", "env_id": "InvertedPendulumBulletEnv-v0"},
+    {"env_name":"Swingup", "env_id": "InvertedPendulumSwingupBulletEnv-v0"},
+    {"env_name":"DoublePendulum","env_id": "InvertedDoublePendulumBulletEnv-v0"},
     ]
 
 def main():
